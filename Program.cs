dca2b9d8c5f4da2817166605b0528b4d3c6d3bf2
cheboi <moscheb96@gmail.com>
2025-03-02 12:44:59 +0300

    update: separation of concern binarySearch

--- Program.cs
+++ Program.cs
@@ -1,28 +1,14 @@
 ﻿using System;
+using Algorithms.Searching;
 
-class BinarySearch
+class Program
 {
-    static int binarySearch(ReadOnlySpan<int> arr, int target)
-    {
-        int left = 0, right = arr.Length - 1;
-
-        while (left <= right)
-        {
-            int mid = left + (right - left) / 2;
-
-            if (arr[mid] == target) return mid;
-            if (arr[mid] < target) left = mid + 1;
-            else right = mid - 1;
-        }
-        return -1; 
-    }
 
     static void Main()
     {
-        int[] numbers = [1, 3, 5, 7, 9, 11, 15]; 
+        int[] arr = [1, 3, 5, 7, 9, 11, 15];
         int target = 7;
-
-        int index = binarySearch(numbers, target);
-        Console.WriteLine(index >= 0 ? $"Found at index {index}" : "Not found");
+        int index = BinarySearch.Search(arr, target);
+        Console.WriteLine(index >= 0 ? $"Binary Search: Found at index {index}" : "Not found");
     }
 }
